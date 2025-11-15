203de2e02807ccec432c6929760de77e33adb720
Greg T. Wallace <greg@gregtwallace.com>
2025-11-15 12:48:30 -0500

    top10/maxheap: add pop top 10

--- src/maxheap/maxheap.go
+++ src/maxheap/maxheap.go
@@ -65,3 +65,71 @@ func (mh *MaxHeap) Insert(key string, weight uint64) {
 		parentIndx = (childIndx - 1) / 2
 	}
 }
+
+// Pop removes and returns the max key: value from the maxHeap
+func (mh *MaxHeap) Pop() (string, uint64) {
+	// get value to return
+	retNode := mh.data[0]
+
+	// remove value and place last value at root
+	mh.data[0] = mh.data[mh.size-1]
+	mh.size--
+
+	// children *2 + 1 ; *2 + 2
+	parentIndx := uint64(0)
+	for {
+		child1Indx := parentIndx*2 + 1
+		child2Indx := parentIndx*2 + 2
+
+		// if first (smaller index child) is out of bounds, done (no children)
+		if child1Indx > mh.size {
+			break
+		}
+
+		// if only one child, check if it is bigger than parent, if so, swap
+		if child2Indx > mh.size {
+			if mh.data[child1Indx].weight > mh.data[parentIndx].weight {
+				tempKey := mh.data[child1Indx].key
+				tempWeight := mh.data[child1Indx].weight
+
+				mh.data[child1Indx].key = mh.data[parentIndx].key
+				mh.data[child1Indx].weight = mh.data[parentIndx].weight
+
+				mh.data[parentIndx].key = tempKey
+				mh.data[parentIndx].weight = tempWeight
+
+				// update parentIndx for next iteration
+				parentIndx = child1Indx
+			} else {
+				// not bigger, done
+				break
+			}
+		} else {
+			// 2 children (compare using whichever child is bigger)
+			compChildIndx := child1Indx
+			if mh.data[child2Indx].weight > mh.data[child1Indx].weight {
+				compChildIndx = child2Indx
+			}
+
+			if mh.data[compChildIndx].weight > mh.data[parentIndx].weight {
+				tempKey := mh.data[compChildIndx].key
+				tempWeight := mh.data[compChildIndx].weight
+
+				mh.data[compChildIndx].key = mh.data[parentIndx].key
+				mh.data[compChildIndx].weight = mh.data[parentIndx].weight
+
+				mh.data[parentIndx].key = tempKey
+				mh.data[parentIndx].weight = tempWeight
+
+				// update parentIndx for next iteration
+				parentIndx = compChildIndx
+			} else {
+				// not bigger, done
+				break
+			}
+		}
+	}
+
+	// done
+	return retNode.key, retNode.weight
+}
--- src/top10.go
+++ src/top10.go
@@ -12,9 +12,6 @@ func top10Words(wordUseHashMap *hashmap.HashMap) {
 
 	// make heap
 	mh := maxheap.NewMaxHeap(27000)
-	// TODO: remove
-	count := uint64(0)
-	//
 
 	it := wordUseHashMap.Begin()
 	tStart := time.Now()
@@ -27,14 +24,25 @@ func top10Words(wordUseHashMap *hashmap.HashMap) {
 		// add to max heap
 		mh.Insert(it.KeyValue())
 
-		count++
-
 		// advance iterator
 		it = it.Next()
 	}
 	tEnd := time.Now()
 	fmt.Printf(" done in %d microseconds.\n", tEnd.Sub(tStart).Microseconds())
 
+	// Print top 10 using the max heap
+	fmt.Printf("Most Used Words (from max heap):\n")
+	tStart = time.Now()
+	for i := range 10 {
+		k, v := mh.Pop()
+		fmt.Printf("%d) %s: %d\n", i+1, k, v)
+	}
+	tEnd = time.Now()
+	fmt.Printf("Max heap took %d microseconds to pop the top 10.\n", tEnd.Sub(tStart).Microseconds())
+
+	// TODO: remove
+	count := uint64(0)
+	//
 	// TODO: Remove
 	fmt.Printf("TODO: REMOVE: iterator count: %d\n", count)
 	//
